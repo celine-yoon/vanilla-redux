2e9838b0acaa3b371cf8783639e2038997b64a2e
Celine Yoon <celine.sy.yoon@gmail.com>
2023-05-12 23:55:26 +0900

    Add: Delete fn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createStore } from "redux/";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-// const ul = document.querySelector("ul");
+const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
@@ -10,7 +10,7 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -25,6 +25,27 @@ store.subscribe(() => console.log("store", store.getState()));
 const addToDo = (text) => {
   store.dispatch({ type: ADD_TODO, text });
 };
+
+const deleteToDo = (event) => {
+  console.log(event.target.parentNode.id);
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteToDo);
+    ul.appendChild(li);
+    li.appendChild(btn);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const onSubmit = (event) => {
   event.preventDefault();
